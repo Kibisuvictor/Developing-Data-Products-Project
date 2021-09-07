d1ee9f1f14afb4de610787617011d74010c87f97
victor kibisu <61618002+Kibisuvictor@users.noreply.github.com>
2021-09-07 19:45:52 +0300

    Add files via upload

--- server.R
+++ server.R
@@ -1,41 +1,26 @@
-library(shiny)
-library(htmlwidgets)
-shinyServer(function(input, output) {
-  values <- reactiveValues()
-  # Calculating the bmi    
-  observe({
-    input$action_Calc
-    if(input$select_measure==1)
-    {
-    values$bmi <- isolate({
-      input$num_weight / ((input$num_height/100) *(input$num_height/100))
-    })
-    }
-    else
-    {
-      values$bmi <- isolate({
-        input$num_weight / (input$num_height *input$num_height)*703
-      })
-    }
-  })
-  
-  # Display values entered
-  output$text_weight <- renderText({
-    input$action_Calc
-    paste("Weight: ", isolate(input$num_weight))
-  })
-  
-  output$text_height <- renderText({
-    input$action_Calc
-    paste("Height: ", isolate(input$num_height))
-  })
-  
-  
-  output$text_bmi <- renderText({
-    if(input$action_Calc == 0) ""
-    else 
-      paste("Your BMI is:", values$bmi)
-  })
-  
-})
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
 
+library(shiny)
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+
+    output$distPlot <- renderPlot({
+
+        # generate bins based on input$bins from ui.R
+        x    <- faithful[, 2]
+        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+        # draw the histogram with the specified number of bins
+        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+
+    })
+
+})
--- ui.R
+++ ui.R
@@ -11,30 +11,30 @@ library(shiny)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("BMI Generator"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-      helpText("Enter your weight and height to know how fit you are!"),  
-      selectInput("select_measure", label = h6("Select the measurement"), choices = list("kg vs. cm" = 1, "lb vs. inches" = 703), selected = 1),
-      numericInput("num_weight", label = h6("Enter your weight"),min = 1, value = NULL),
-      numericInput("num_height", label = h6("Enter your height"),min = 1, value = NULL),
-      actionButton("action_Calc", label = "CALCULATE")        
-    ),
-    mainPanel(
-      tabsetPanel(
-        tabPanel("BMI",
-                 p(h5("")), textOutput("text_weight"),
-                 textOutput("text_height"),  p(h5("Body Mass Index(BMI):")),
-                 textOutput("text_bmi")
+    
+    # Application title
+    titlePanel("BMI Generator"),
+    
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+            helpText("Enter your weight and height to know how fit you are!"),  
+            selectInput("select_measure", label = h6("Select the measurement"), choices = list("kg vs. cm" = 1, "lb vs. inches" = 703), selected = 1),
+            numericInput("num_weight", label = h6("Enter your weight"),min = 1, value = NULL),
+            numericInput("num_height", label = h6("Enter your height"),min = 1, value = NULL),
+            actionButton("action_Calc", label = "CALCULATE")        
         ),
-        tabPanel("BMI Chart",
-                 p(h4("Simple Interest Calculator:")),
-                 helpText("Statistical Categories of BMI as given by WHO"),
-                 HTML("
+        mainPanel(
+            tabsetPanel(
+                tabPanel("BMI",
+                         p(h5("")), textOutput("text_weight"),
+                         textOutput("text_height"),  p(h5("Body Mass Index(BMI):")),
+                         textOutput("text_bmi")
+                ),
+                tabPanel("BMI Chart",
+                         p(h4("Simple Interest Calculator:")),
+                         helpText("Statistical Categories of BMI as given by WHO"),
+                         HTML("
   
                       <b> Less than 15 </b> = Very Severly Underweight <br>
                       <br> </br>
@@ -51,8 +51,8 @@ shinyUI(fluidPage(
                       <b> Between 35 and 40 </b> = Severely Overweight <br>
                       <br> </br>
                       <b> Above 40 </b> = Very Severely Overweight <br>")                
-                 )
-                 )
-                 )
-                 )
-                 ))
+                )
+            )
+        )
+    )
+))
